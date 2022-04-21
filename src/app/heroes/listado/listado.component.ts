d4f8f0ab9c062c7b76a0f9e9b289baf339766316
Francisco Vazquez <fjvazquez85@gmail.com>
2022-04-21 16:06:13 +0200

    Primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,22 @@
+import { NgIf } from '@angular/common';
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+ 
+})
+export class ListadoComponent {
+
+  heroes: string[] = ['Spiderman','Ironman','Hulk','Thor','Captain America'];
+  heroeBorrado: string = '';
+
+
+  borrarHeroe(){
+    this.heroeBorrado=this.heroes.shift() || '';
+     
+  
+  }
+
+}
+
